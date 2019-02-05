862f8e0d37864a8c5cf6e200ab0a028db352e3ce
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-02-04 17:24:15 -0800

    api hooked up to applicant detail

--- src/api.js
+++ src/api.js
@@ -1,12 +1,14 @@
 const api = {
     save: function(applicant) {
         const serialize = JSON.stringify(applicant);
+        //console.log serialize
         window.localStorage.setItem('applicant', serialize);
     },
 
     get: function() {
         const json = window.localStorage.getItem('applicant');
         const hydrate = JSON.parse(json);
+        //console.log hydrate
         return hydrate;
     }
 };
---
+++ src/applicant-detail/applicant-detail.js
@@ -0,0 +1,12 @@
+import api from '../api.js';
+const name = document.getElementById('name');
+const position = document.getElementById('position');
+const peanuts = document.getElementById('peanuts');
+const tigerNames = document.getElementById('tiger-names');
+
+const applicant = api.get();
+
+name.textContent = applicant.name;
+position.textContent = applicant.position;
+peanuts.textContent = applicant.peanuts;
+tigerNames.textContent = applicant.tigerNames.join(', ');
